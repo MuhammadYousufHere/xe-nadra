7383dadc526bc60035d7f12e8e24dc0b7d7db09e
Muhammad-Yousuf <khanyousufmy80@gmail.com>
2022-12-10 01:38:15 +0500

    initial commit

--- src/server.js
+++ src/server.js
@@ -28,32 +28,7 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the application.' });
 });
 
-//   const { email, password, mobileNum } = req.body;
-//   const user = {
-//     email,
-//     password,
-//     mobileNum,
-//   };
-
-//   const welcomeMessage = 'Your verification code is 54875';
-
-//   try {
-//     sendSms(user.mobileNum, welcomeMessage);
-//     res.status(200).send({
-//       message:
-//         'Account created successfully, kindly check your phone to activate your account!',
-//       data: user,
-//       success: true,
-//     });
-//   } catch (error) {
-//     res.status(500).send({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// });
-
-app.use('/users', router); // path must route to lambda
+app.use('/users', router);
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
